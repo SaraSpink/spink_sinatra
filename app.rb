e7bdbf516d0b6da3c315bb185d1f02160400148f
Sara Spink <spinkbot@gmail.com>
2017-09-15 16:31:21 -0700

    you can enter a word but on the definition page it errors

--- app.rb
+++ app.rb
@@ -9,20 +9,47 @@ require ('pry')
     erb(:input)
   end
 
+  post('/') do
+    @word_app = params["word"]
+    @list_app = Word.all()
+    word_info = Word.new({:word=> @word_app, :definition=> @definition_app})
+
+    word_info.save
+    @list = Word.all()
+    erb(:input)
+  end
+
   get('/define/:id') do
-  
     @contact_id = Word.find(params[:id])
     @list = Word.all()
     erb(:definition)
   end
 
-  post('/') do
+  post('/define/:id') do
     @word_app = params["word"]
     @definition_app = params["definition"]
+    save_and_open_page
     @list_app = Word.all()
-    contact_info = Word.new({:word=> @word_app, :definition=> @definition_app})
+    word_info = Word.new({:word=> @word_app, :definition=> @definition_app})
 
-    contact_info.save
+    word_info.save
     @list = Word.all()
-    erb(:input)
+    erb(:definition)
   end
+
+  # get('/yourword/:id') do
+  #   @contact_id = Word.find(params[:id])
+  #   @list = Word.all()
+  #   erb(:yourword)
+  # end
+  #
+  # post('/yourword/:id') do
+  #   @word_app = params["word"]
+  #   @definition_app = params["definition"]
+  #   @list_app = Word.all()
+  #   word_info = Word.new({:word=> @word_app, :definition=> @definition_app})
+  #
+  #   word_info.save
+  #   @list = Word.all()
+  #   erb(:yourword)
+  # end
